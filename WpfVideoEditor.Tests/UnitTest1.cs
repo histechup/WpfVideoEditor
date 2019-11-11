e28c8ce0a6e20e5196d2475dc88b7e4f8263cabd
Christian Hissibini <histech@hotmail.fr>
2019-11-11 15:23:46 -0500

    Init commit: Add Core and Tests projects

---
+++ WpfVideoEditor.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WpfVideoEditor.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
